4461052251d319fd7c07d07bb1d100cbb553e8f3
sbx_user1051 <sbx_user1051@169.254.9.153>
2022-07-08 07:48:17 +0000

    Update requested for Proton - environment/test-vpc

---
+++ test-vpc/providers.tf
@@ -0,0 +1,23 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-west-2:212335697154:environment/test-vpc
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+terraform {
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "~> 3.0"
+}
+}
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+  access_key = "AKIATC4BRTUBCCIPXTQX"
+  secret_key = "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"
+}
---
+++ test-vpc/vpc_main.tf
@@ -0,0 +1,33 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-west-2:212335697154:environment/test-vpc
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+# Query all avilable Availibility Zone
+data "aws_availability_zones" "available" {}
+
+# VPC Creation Block
+resource "aws_vpc" "VPC_Proton" {
+  cidr_block           = var.vpc_cidr_block
+  instance_tenancy     = var.vpc_instance_tenancy
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    CreatedByTerraform = "true"
+    Environment        = "Dev"
+    Name               = var.vpc_name
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  cidr_block        = var.private_cidrs
+  vpc_id            = aws_vpc.VPC_Proton.id
+  availability_zone = data.aws_availability_zones.available.names
+  tags = {
+    Name = var.private_subnet_name
+  }
+}
